31e77de3df8570bbe353a16faa8aeafbc3a42303
marcosuasv <marco.suastegui@gbpo.com.mx>
2026-06-26 13:45:45 -0600

    modificaciones 26-06-2026

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*'); // <-- Añade esta línea
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();

1101826a6843e0662e7f8cbae4218f083ca35a3e
marcosuasv <marco.suastegui@gbpo.com.mx>
2026-06-26 13:58:29 -0600

    modificaciones 26-06-2026 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\Abono;
 use App\Observers\AbonoObserver;
 
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Abono::observe(AbonoObserver::class);
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
        
     }
 
